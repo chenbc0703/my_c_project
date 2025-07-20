3fef54f4bbb574235c403cf6ba0e5fcff9839822
chenbc0703 <chenbc0703@gmail.com>
2025-07-20 10:09:36 +0100

    bb add

--- test.c
+++ test.c
@@ -6,5 +6,8 @@ int main() {
     printf_m("Hello, Git with C!\n");
 
     printf_m("Hello, Git with C!\n");
+
+    printf_bb("Hello, Git with C!\n");
+    printf_bb("Hello, Git with C!\n");
     return 0;
 }
